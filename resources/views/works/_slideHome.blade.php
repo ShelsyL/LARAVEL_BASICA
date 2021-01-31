a3c234c508ffefd778ffd2baf0d074caa4da3bd0
Chelsea <chelsealouis.graphiste@gmail.com>
2021-01-31 20:51:20 +0100

    Mise en place du Slider de la page Home

--- resources/views/works/_slideHome.blade.php
+++ resources/views/works/_slideHome.blade.php
@@ -1,26 +1,13 @@
 <section id="main-slider" class="no-margin">
    <div class="carousel slide">
       <ol class="carousel-indicators">
-         <li data-target="#main-slider" data-slide-to="0" class="active"></li>
-         <li data-target="#main-slider" data-slide-to="1"></li>
-         <li data-target="#main-slider" data-slide-to="2"></li>
+        @foreach ($works as $work)
+          <li data-target="#main-slider" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
+        @endforeach
       </ol>
       <div class="carousel-inner">
-         <div class="item active" style="background-image: url({{ asset('assets/img/slides/1.jpg') }})">
-            <div class="container">
-               <div class="row">
-                  <div class="col-sm-12">
-                     <div class="carousel-content centered">
-                        <h2 class="animation animated-item-1">Welcome to BASICA! A Bootstrap3 Template</h2>
-                        <p class="animation animated-item-2">Sed mattis enim in nulla blandit tincidunt. Phasellus vel sem convallis, mattis est id, interdum augue. Integer luctus massa in arcu euismod venenatis. </p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <!--/.item-->
          @foreach ($works as $work)
-           <div class="item" style="background-image: url({{ asset('assets/img/slides/' . $work->image) }}">
+           <div class="item {{ $loop->first ? 'active' : '' }}" style="background-image: url({{ asset('assets/img/slides/' . $work->image) }})">
               <div class="container">
                  <div class="row">
                     <div class="col-sm-12">
@@ -36,11 +23,13 @@
            </div>
            <!--/.item-->
          @endforeach
-         <!--/.item-->
+
       </div>
       <!--/.carousel-inner-->
    </div>
    <!--/.carousel-->
+
+   
    <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
      <i class="icon-angle-left"></i>
    </a>
